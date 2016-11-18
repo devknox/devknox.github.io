e25297da0c306ea9b95995c69206b42e0cf5a7d5
yashwin <yashwinpoojary@gmail.com>
2016-11-18 17:06:37 +0530

    feature: implement slide up and down feature

--- js/startuply.js
+++ js/startuply.js
@@ -1,2 +1,12 @@
+$( document ).ready(function() {
+  $(".slide-down").click(function(){
+      $(".show-document").slideDown();
+      $('.slide-down').hide();
+      $('.slide-up').show();
+    });
+  $(".slide-up").click(function(){
+    $(".show-document").slideUp();
+    $('.slide-up').hide();
+    $('.slide-down').show();
   });
 });
