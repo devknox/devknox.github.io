c0b9ccd0e78d5d4bf6ad6ff69fe820e175c1b51b
yashwin <yashwinpoojary@gmail.com>
2016-11-23 01:05:08 +0530

    refactor: well organise js code

--- js/startuply.js
+++ js/startuply.js
@@ -1,29 +1,8 @@
 $( document ).ready(function() {
-  $('.doc-slide-down').click(function(){
-      $('.doc-show-document').slideDown();
-      $('.doc-slide-down').hide();
-      $('.doc-slide-up').show();
-    });
-  $('.doc-slide-up').click(function(){
-    $('.doc-show-document').slideUp();
-    $('.doc-slide-up').hide();
-    $('.doc-slide-down').show();
-  });
-  $('.faq-slide-down').click(function(){
-      $('.faq-show-document').slideDown();
-      $('.faq-slide-down').hide();
-      $('.faq-slide-up').show();
-    });
-  $('.faq-slide-up').click(function(){
-    $('.faq-show-document').slideUp();
-    $('.faq-slide-up').hide();
-    $('.faq-slide-down').show();
-  });
-  $('.faq-slide-up').click(function(){
-    $('.faq-show-document').slideUp();
-    $('.faq-slide-up').hide();
-    $('.faq-slide-down').show();
-  });
+    faqControl();
+    docControl();
+
+
   $('.pricing-start-button').click(function(){
     $('.devknox-pricing-lite').hide();
     $('.devknox-pricing').hide();
@@ -50,3 +29,48 @@ $('#register-form-link').click(function(e) {
 });
 
 });
+
+faqControl = function(){
+  $('.faq-quest-area').on('click touchend', function(ev) {
+    var faqEle = $(ev.currentTarget).parent();
+    var icon = $(ev.currentTarget).parent().find("i");
+    var displayCurEle = faqEle.css("display");
+    $(".faq-answer").each(function(){
+      if($(this).parent()[0] !== faqEle[0]){
+        $(this).stop().slideUp(400);
+        if($(this).parent().find("i").hasClass("fa-caret-down")){
+          $(this).parent().find("i").toggleClass("fa-caret-right fa-caret-down");
+        }
+      }
+    });
+
+    faqEle.find(".faq-answer").slideToggle(400);
+
+    var isClosed = icon.hasClass("fa-caret-right");
+    icon.toggleClass("fa-caret-right fa-caret-down");
+
+  });
+  $('.faq-slide-down').click(function(){
+      $('.faq-show-document').slideDown();
+      $('.faq-slide-down').hide();
+      $('.faq-slide-up').show();
+    });
+  $('.faq-slide-up').click(function(){
+    $('.faq-show-document').slideUp();
+    $('.faq-slide-up').hide();
+    $('.faq-slide-down').show();
+  });
+}
+
+docControl = function(){
+  $('.doc-slide-down').click(function(){
+      $('.doc-show-document').slideDown();
+      $('.doc-slide-down').hide();
+      $('.doc-slide-up').show();
+    });
+  $('.doc-slide-up').click(function(){
+    $('.doc-show-document').slideUp();
+    $('.doc-slide-up').hide();
+    $('.doc-slide-down').show();
+  });
+}
