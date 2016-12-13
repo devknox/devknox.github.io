8967279b040e63bebce24db76a21330ad6a28e51
yashwin <yashwinpoojary@gmail.com>
2016-12-13 19:53:38 +0530

    fix: menu color changes

--- js/main.js
+++ js/main.js
@@ -22,78 +22,82 @@ $(document).ready(function() {
     $('.pricing-type-lite').hide();
 
   });
+
   $('.close-button').click(function(){
     $('.devknox-pricing-lite').show();
     $('.devknox-pricing').show();
     $('.submission-form').hide();
     $('.om-section').hide();
   });
+
   $('#login-form-link').click(function(e) {
   $("#login-form").delay(100).fadeIn(100);
   $("#register-form").fadeOut(100);
   $('#register-form-link').removeClass('active');
   $(this).addClass('active');
   e.preventDefault();
-});
-$('#register-form-link').click(function(e) {
-  $("#register-form").delay(100).fadeIn(100);
-  $("#login-form").fadeOut(100);
-  $('#login-form-link').removeClass('active');
-  $(this).addClass('active');
-  e.preventDefault();
-});
 
-$('#signup-form').validate({
-  submitHandler: function (form) {
-    $.ajax({
-      url: "https://hawkins.appknox.com/api/devknox_register/",
-      type: 'POST',
-      dataType: 'json',
-      data: $(form).serialize()
-      }).done(function (msg) {
-          if(msg.status === "error") {
-              $("#signup-form :input").attr("disabled", false);
-              toastr.error(msg.message);
-              if(msg.message.startsWith("Password")) {
-                  validator.showErrors({
-                      "password": msg.message,
-                      "confirmPassword": msg.message
-                  });
-              }
-              if(msg.message.startsWith("Username")) {
-                  validator.showErrors({"username": msg.message});
-              }
-          } else {
-              $(this).trigger("reset");
-              $('.submission-form').hide();
-              $('.om-section').show();
-          }
-      }).fail(function() {
-          toastr.error("Something went wrong");
-          $("signup-form :input").attr("disabled", false);
-      });
-   return false;
- }
-});
+  });
 
-$('#getin_touch').validate({
-  submitHandler: function (form) {
-    $.ajax({
-      url: "",
-      type: 'POST',
-      dataType: 'json',
-      data: $(form).serialize()
-      }).done(function (msg) {
-        $('.footer-heading').hide();
-        $('#getin_touch').hide();
-        $('.thanks-message').show();
-      }).fail(function() {
+  $('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
 
-      });
-   return false;
+  $('#signup-form').validate({
+    submitHandler: function (form) {
+      $.ajax({
+        url: "https://hawkins.appknox.com/api/devknox_register/",
+        type: 'POST',
+        dataType: 'json',
+        data: $(form).serialize()
+        }).done(function (msg) {
+            if(msg.status === "error") {
+                $("#signup-form :input").attr("disabled", false);
+                toastr.error(msg.message);
+                if(msg.message.startsWith("Password")) {
+                    validator.showErrors({
+                        "password": msg.message,
+                        "confirmPassword": msg.message
+                    });
+                }
+                if(msg.message.startsWith("Username")) {
+                    validator.showErrors({"username": msg.message});
+                }
+            } else {
+                $(this).trigger("reset");
+                $('.submission-form').hide();
+                $('.om-section').show();
+            }
+        }).fail(function() {
+            toastr.error("Something went wrong");
+            $("signup-form :input").attr("disabled", false);
+        });
+     return false;
     }
   });
-});
+
+  $('#getin_touch').validate({
+    submitHandler: function (form) {
+      $.ajax({
+        url: "",
+        type: 'POST',
+        dataType: 'json',
+        data: $(form).serialize()
+        }).done(function (msg) {
+          $('.footer-heading').hide();
+          $('#getin_touch').hide();
+          $('.thanks-message').show();
+        }).fail(function() {
+
+        });
+     return false;
+      }
+    });
+  });
 
 faqControl = function(){
   $('.faq-quest-area').on('click touchend', function(ev) {
