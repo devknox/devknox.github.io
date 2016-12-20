06aa93c6ec2591fea74546b6cf46e301dfde2ec3
yashwin <yashwinpoojary@gmail.com>
2016-12-20 13:36:40 +0530

    fix: send is paid data

--- js/main.js
+++ js/main.js
@@ -49,13 +49,13 @@ $(document).ready(function() {
 
   $('#signup-form').validate({
     submitHandler: function (form) {
-      var formData = $(form).serialize();
-      paidPricing = isPaid;
+      var formData = $(form).serializeArray();
+      formData.push({name: "isPaid", value: isPaid});
       $.ajax({
         url: "https://hawkins.appknox.com/api/devknox_register/",
         type: 'POST',
         dataType: 'json',
-        data: formData + "Paid = " + paidPricing
+        data: $.param(formData)
         }).done(function (msg) {
             if(msg.status === "error") {
                 $("#signup-form :input").attr("disabled", false);
